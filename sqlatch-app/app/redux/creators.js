0f88e20a50b7f36ee8999d7439c0df3e7148d5d0
vcollado <necseron@gmail.com>
2016-08-09 18:46:12 +0200

    Código de pruebas eliminado, primera implementación funcional de añadido/eliminación de nodos funcionando

--- sqlatch-app/app/redux/creators.js
+++ sqlatch-app/app/redux/creators.js
@@ -1,19 +1,23 @@
 let sqlatch   = sqlatch || {};
 sqlatch.redux = sqlatch.redux || {};
-let types     = sqlatch.redux.types;
+let {
+   ADD_QUERY_NODE, 
+   REMOVE_QUERY_NODE, 
+   VALIDATE_QUERY_CONTENT
+} = sqlatch.redux.types;
 
-function add_node(node){
-   return {type : types.ADD_NODE, node};
+function add_query_node(node){
+   return {type: ADD_QUERY_NODE, node};
 }
 
-function remove_node(id){
-   return {type : types.REMOVE_NODE, id};
+function remove_query_node(id){
+   return {type: REMOVE_QUERY_NODE, id}
 }
 
 function validate_query_content(is_valid){
-   return {type : types.VALIDATE_QUERY_CONTENT, is_valid};
+   return {type : VALIDATE_QUERY_CONTENT, is_valid};
 }
 
 sqlatch.redux.creators = {
-   add_node, remove_node, validate_query_content
+   add_query_node, remove_query_node, validate_query_content
 };
--- sqlatch-app/app/redux/types.js
+++ sqlatch-app/app/redux/types.js
@@ -1,10 +1,10 @@
 let sqlatch   = sqlatch || {};
 sqlatch.redux = sqlatch.redux || {}; 
 
-const ADD_NODE               = 'ADD_NODE';
-const REMOVE_NODE            = 'REMOVE_NODE';
+const ADD_QUERY_NODE         = 'ADD_NODE';
+const REMOVE_QUERY_NODE      = 'REMOVE_NODE';
 const VALIDATE_QUERY_CONTENT = 'VALIDATE_QUERY_CONTENT';
 
 sqlatch.redux.types = {
-   ADD_NODE, REMOVE_NODE, VALIDATE_QUERY_CONTENT
+   ADD_QUERY_NODE, REMOVE_QUERY_NODE, VALIDATE_QUERY_CONTENT
 };
