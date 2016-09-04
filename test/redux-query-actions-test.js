759950acc42461430b7d4e608e90898f82c80972
vcollado <necseron@gmail.com>
2016-09-04 17:20:37 +0200

    flux standard action test added

--- test/redux-query-actions-test.js
+++ test/redux-query-actions-test.js
@@ -1,9 +1,10 @@
-import expect from 'expect'
+import expect from 'expect';
+import { isFSA } from 'flux-standard-action';
 
 import {ADD_QUERY_NODE} from '../src/redux/query/types';
-import {add_query_node} from '../src/redux/query/actions';
+import {add_query_node} from '../src/redux/query/actions-creator';
 
-describe('redux query actions', () => {
+describe('redux query actions creator', () => {
 	it('should create an action to add a query node', () => {			
 		// force commit
 		const node_to_add = {
@@ -11,8 +12,16 @@ describe('redux query actions', () => {
 		};
 		const expected_action = {
 			type : ADD_QUERY_NODE,
-			node : node_to_add
+			payload : {
+				node : node_to_add
+			}
 		};
 		expect(add_query_node(node_to_add)).toEqual(expected_action);
+	});
+});
+
+describe('redux query actions creators Flux Standard Action compliant', ()=> {
+	it('action add a query node should be FSA compliant', () =>{
+		expect(isFSA(add_query_node())).toBe(true); 
 	});
 });
