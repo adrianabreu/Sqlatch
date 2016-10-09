597d67dad66823708762190993a4988769eaa166
vcollado <necseron@gmail.com>
2016-10-09 22:58:59 +0200

    try to fix flux-standard-action dev. dep and fixed tests

--- test/redux-query-actions-test.js
+++ test/redux-query-actions-test.js
@@ -8,13 +8,12 @@ describe('redux query actions creator', () => {
 	it('should create an action to add a query node', () => {			
 		// force commit
 		const node_to_add = {
-			id : 999
+				query_id   : 999,
+				node :{}
 		};
 		const expected_action = {
 			type : ADD_QUERY_NODE,
-			payload : {
-				node : node_to_add
-			}
+			payload : node_to_add
 		};
 		expect(add_query_node(node_to_add)).toEqual(expected_action);
 	});
@@ -22,6 +21,10 @@ describe('redux query actions creator', () => {
 
 describe('redux query actions creators Flux Standard Action compliant', ()=> {
 	it('action add a query node should be FSA compliant', () =>{
-		expect(isFSA(add_query_node())).toBe(true); 
+		// todo... check function call without parameters
+		expect(isFSA(add_query_node({
+			query_id : 0,
+			node 	 : {}
+		}))).toBe(true); 
 	});
 });
